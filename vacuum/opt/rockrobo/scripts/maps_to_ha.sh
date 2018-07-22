8bf74df78cd49a225d685d470e358d5051c73b12
ewilly <ewilly@neuf.fr>
2018-07-22 15:33:52 +0200

    Prefer fullpath for RDIR
    if RUSER (=SSHUSER) is not the same as HA USER (generally
    homeassistant), ~ point to a wrong destination

--- vacuum/opt/rockrobo/scripts/maps_to_ha.sh
+++ vacuum/opt/rockrobo/scripts/maps_to_ha.sh
@@ -6,7 +6,7 @@ RPORT="22"
 ### You shouldn't touch anything below ###
 
 RUSER="homeassistant"
-RDIR="vacuum"
+RDIR="/home/homeassistant/vacuum/"
 SSHCTL="/root/.ssh/ctl-ha-rockrobo"
 
 while true; do
@@ -16,7 +16,7 @@ while true; do
             ssh -p ${RPORT} -qnNf -o ControlMaster=yes -o ControlPath="${SSHCTL}" ${RUSER}@${RHOST}
         fi
         # rsync will handle sparse files to let navmaps be transferred quickier
-        rsync -cqrlDS -e "ssh -p ${RPORT} -o 'ControlPath=${SSHCTL}'" --timeout 10 --include="navmap*.ppm" --include="SLAM_fprintf.log" --exclude="*" /run/shm/ ${RUSER}@${RHOST}:~/${RDIR}/
+        rsync -cqrlDS -e "ssh -p ${RPORT} -o 'ControlPath=${SSHCTL}'" --timeout 10 --include="navmap*.ppm" --include="SLAM_fprintf.log" --exclude="*" /run/shm/ ${RUSER}@${RHOST}:${RDIR}
     else
         # If the robot isn't moving then exit from the SSH control path session (if exists) and sleep waiting for the next loop
         if [ -e ${SSHCTL} ]; then
